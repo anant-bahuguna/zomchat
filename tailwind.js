3176448ea2d3bfec565d6cf198db1a7818496330
anant-bahuguna <anantbahugunaddn@gmail.com>
2020-08-29 22:57:22 +0530

    basic sidebar complete

--- tailwind.js
+++ tailwind.js
@@ -16,6 +16,9 @@ module.exports = {
                 "gray-750": "#3f495a",
                 "gray-850": "#222733",
             },
+            fontSize: {
+                xxs: "0.5rem",    
+            }
         },
         colors: {
             transparent: "transparent",
